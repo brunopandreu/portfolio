ffcf8cffbe2111f67b275d6a08f0384458f0ba64
bruno <brunop.andreu@hotmail.com>
2022-12-14 17:44:17 -0300

    estrutura da página home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { SideBarComponent } from './pages/components/side-bar/side-bar.component';
+import { BannerComponent } from './pages/components/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    SideBarComponent,
+    BannerComponent,
     
   ],
   imports: [
